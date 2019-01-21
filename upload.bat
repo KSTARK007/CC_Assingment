ff13c8da31f0983861f6dcc3826141d1984d4f85
KSTARK007 <kiranhombal98@gmail.com>
2019-01-21 19:31:20 +0530

    kiran

--- upload.bat
+++ upload.bat
@@ -1,21 +1,27 @@
 @echo off
 echo updating the github account enter the update string
-echo.
-echo.
 SET /P v=
+echo.
+echo.
+echo.
+echo.
 git status
 echo.
 echo.
 echo.
 pause
+cls
 git add .
 echo added
 echo.
 echo commiting
 echo.
+pause
+cls
 git commit -m %v%
 echo uploading
 echo.
 pause
+cls
 git push
 pause
